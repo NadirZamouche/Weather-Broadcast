59e0f0f044b2e35f4c53e59e99dbc5b5f812ffd0
Nadir Zamouche <95188070+FlammeTik@users.noreply.github.com>
2024-06-11 22:46:29 +0100

    push

--- src/App.js
+++ src/App.js
@@ -9,11 +9,16 @@ function App() {
   const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState(null);
 
+  const handleSetWeatherData = (data) => {
+    setWeatherData(data);
+    setError(null); // Clear the error when new weather data is set
+  };
+
   return (
     <div className="App">
       <img src={weatherImage} alt="Weather" className="weather-image" />
       <h1>Weather App</h1>
-      <SearchBar setWeatherData={setWeatherData} setError={setError} />
+      <SearchBar setWeatherData={handleSetWeatherData} setError={setError} />
       {error && <div className="error-message">{error}</div>}
       {weatherData && !error && <Weather weatherData={weatherData} />}
     </div>
